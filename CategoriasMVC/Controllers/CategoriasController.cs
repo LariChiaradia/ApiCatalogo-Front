707327171698aae13c5bf7e3607e0871d19c2574
Larissa Chiaradia Da Silva <larissa.silva@fcamara.com>
2024-05-06 18:02:53 -0300

    feat: Criado o método Action e a View para Deletar a categoria

--- CategoriasMVC/Controllers/CategoriasController.cs
+++ CategoriasMVC/Controllers/CategoriasController.cs
@@ -30,11 +30,11 @@ namespace CategoriasMVC.Controllers
         [HttpPost]
         public async Task<ActionResult<CategoriaViewModel>> CriarNovaCategoria(CategoriaViewModel categoriaVM)
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 var result = await _categoriaService.CriarCategoria(categoriaVM);
 
-                if(result != null)
+                if (result != null)
                     return RedirectToAction(nameof(Index));
             }
             ViewBag.Erro = "Erro ao criar Categoria";
@@ -52,7 +52,7 @@ namespace CategoriasMVC.Controllers
         [HttpPost]
         public async Task<ActionResult<CategoriaViewModel>> AtualizarCategoria(int id, CategoriaViewModel categoriaVM)
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 var result = await _categoriaService.AtualizarCategoria(id, categoriaVM);
 
@@ -61,6 +61,26 @@ namespace CategoriasMVC.Controllers
             }
             ViewBag.Erro = "Erro ao atualizar Categoria";
             return View(categoriaVM);
+        }
+        [HttpGet]
+        public async Task<ActionResult> DeletarCategoria(int id)
+        {
+            var result = await _categoriaService.GetCategoriaPorId(id);
+
+            if (result is null)
+                return View("Error");
+
+            return View(result);
+        }
+        [HttpPost(), ActionName("DeletarCategoria")]
+        public async Task<IActionResult> DeletarConfirmado(int id)
+        {
+            var result = await _categoriaService.DeletaCategoria(id);
+
+            if (result)
+                return RedirectToAction(nameof(Index));
+
+            return View("Error");
         }
     }
 }

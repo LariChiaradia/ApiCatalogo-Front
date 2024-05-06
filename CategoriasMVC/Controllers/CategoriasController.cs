9ac4205d7f1c0879667b3b0dd99ee1791d689742
Larissa Chiaradia Da Silva <larissa.silva@fcamara.com>
2024-05-06 17:11:02 -0300

    feat: Criado o método Action e View para atualizar a categoria

--- CategoriasMVC/Controllers/CategoriasController.cs
+++ CategoriasMVC/Controllers/CategoriasController.cs
@@ -40,5 +40,27 @@ namespace CategoriasMVC.Controllers
             ViewBag.Erro = "Erro ao criar Categoria";
             return View(categoriaVM);
         }
+        [HttpGet]
+        public async Task<IActionResult> AtualizarCategoria(int id)
+        {
+            var result = await _categoriaService.GetCategoriaPorId(id);
+            if (result is null)
+                return View("Error");
+
+            return View(result);
+        }
+        [HttpPost]
+        public async Task<ActionResult<CategoriaViewModel>> AtualizarCategoria(int id, CategoriaViewModel categoriaVM)
+        {
+            if(ModelState.IsValid)
+            {
+                var result = await _categoriaService.AtualizarCategoria(id, categoriaVM);
+
+                if (result)
+                    return RedirectToAction(nameof(Index));
+            }
+            ViewBag.Erro = "Erro ao atualizar Categoria";
+            return View(categoriaVM);
+        }
     }
 }

183d90c4a6816cf8b3791c56394033c8892bd743
Larissa Chiaradia Da Silva <larissa.silva@fcamara.com>
2024-05-06 16:07:30 -0300

    feat: Primeiro Commit do Projeto

---
+++ CategoriasMVC/Controllers/CategoriasController.cs
@@ -0,0 +1,26 @@
+﻿using CategoriasMVC.Models;
+using CategoriasMVC.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CategoriasMVC.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        private readonly ICategoriaService _categoriaService;
+
+        public CategoriasController(ICategoriaService categoriaService)
+        {
+            _categoriaService = categoriaService;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CategoriaViewModel>>> Index()
+        {
+            var result = await _categoriaService.GetCategorias();
+
+            if (result is null)
+                return View("Error");
+
+            return View(result);
+        }
+    }
+}

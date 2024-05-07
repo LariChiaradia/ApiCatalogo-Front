45742d75742818a2665b47657c39a219824f6dde
Larissa Chiaradia Da Silva <larissa.silva@fcamara.com>
2024-05-07 09:42:17 -0300

    feat: Criadas as Models de Produto, Token e Usuário

---
+++ CategoriasMVC/Models/ProdutoViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CategoriasMVC.Models
+{
+    public class ProdutoViewModel
+    {
+        public int ProdutoId { get; set; }
+        [Required(ErrorMessage = "O nome do produto é obrigatório")]
+        public string? Nome { get; set; }
+        [Required(ErrorMessage = "A descrição é obrigatória")]
+        public string? Descricao { get; set; }
+        [Required(ErrorMessage = "O preço é obrigatório")]
+        public double Preco { get; set; }
+        [Required]
+        [Display(Name = "Imagem")]
+        public string? ImageUrl { get; set; }
+        [Required(ErrorMessage = "É obrigatório informar o ID da categoria.")]
+        public int CategoriaId { get; set; }
+    }
+}
---
+++ CategoriasMVC/Models/TokenViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CategoriasMVC.Models
+{
+    public class TokenViewModel
+    {
+        public bool Authenticate { get; set; }
+        public DateTime Expiration { get; set; }
+        public string? Token { get; set; }
+        public string? Message { get; set; }
+    }
+}
